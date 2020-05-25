50ec648bf7edebb9c63ed29e8512a37deb1de692
Aaron Southammavong <aaroneight07@gmail.com>
2020-05-25 15:31:25 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData} />
+        <About resumeData={resumeData} />
+        <Portfolio resumeData={resumeData} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+  imagebaseurl: 'https://github.com/aaronsomo',
+  name: 'Aaron Southammavong',
+  role: 'Full-Stack Mobile and Web Developer',
+  linkedinId: ' https://linkedin.com/aaronsouthammavong',
+  // skypeid: 'Your skypeid',
+  roleDescription:
+    '  I enjoy solving problems that challenge my understanding, as well as engaging myself in finding better solutions. With that said, I’d love the opportunity to learn more about your challenges and have a discussion about what I can do to help solve them.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/aaronsouthammavong/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'http://github.com/aaronsomo',
+      className: 'fa fa-github',
+    },
+    {
+      name: 'twitter',
+      url: 'http://twitter.com/aaroneksouth',
+      className: 'fa fa-twitter',
+    },
+  ],
+  aboutme:
+    "Welcome! I'm Aaron and I have a knack for building creative and meaningful things, be it a bridge in Chicago or a web application on the Internet. As a former Civil Engineer, I’ve worked with different teams to utilize my creative nature in designing and managing many projects, but they haven’t quite filled that void - that curiosity in seeing an idea, solving issues, and having that idea realized. Software Engineering has me expressing that passion, as I take on more challenges, and I couldn't be happier.",
+  address: 'Los Angeles',
+  website: 'https://github.com/aaronsomo',
+  education: [
+    {
+      UniversityName: 'Hack Reactor',
+      specialization: 'Software Engineering Immersive',
+      MonthOfPassing: 'January',
+      YearOfPassing: '2020',
+      // Achievements: 'Some Achievements',
+    },
+    {
+      UniversityName: 'University of Illinois at Chicago',
+      specialization: 'Bachelor of Science in Civil Engineering',
+      MonthOfPassing: 'May',
+      YearOfPassing: '2013',
+      // Achievements: 'Some Achievements',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'SMRTLENS.COM',
+      specialization: 'CEO/Co-Founder',
+      // MonthOfLeaving: 'Jan',
+      // YearOfLeaving: '2018',
+      Achievements:
+        "Designing and implementing business operations. Providing a means of protecting peoples' eyes from technologies that emit blue-light. Managing a small team that handles order processing and distribution.",
+    },
+    {
+      CompanyName: 'Infrastructure Engineering, Inc',
+      specialization: 'Civil Engineer',
+      MonthOfLeaving: 'September',
+      YearOfLeaving: '2019',
+      Achievements:
+        'A part of a team that managed the construction of a safe pedestrian bridge path/walkway for the community that alleviated pedestrian congestion during peak hours. A part of a team that installed a community park for a neighborhood. Redesigned hundreds of pedestrian sidewalks and intersections to accommodate ADA standards and address drainage problems, among other issues.',
+    },
+    {
+      CompanyName: 'Illinois Department of Transportation',
+      specialization: 'Civil Engineer',
+      MonthOfLeaving: 'March',
+      YearOfLeaving: '2013',
+      Achievements:
+        'Assistant Resident Engineer at Lisbon Road Bridge. Managed the construction of the bridge. Alleviated congestion during peak hours. Provided a safe route of travel for pedestrians. Assistant Resident Engineer at Minooka Road bridge. Co-managed bridge to accommodate 4 lanes of travel in anticipation of highway expansion, traffic, and neighborhood growth. A part of a team that designed infrastructure projects for District 3 in the State of Illinois.',
+    },
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5',
+    },
+    {
+      skillname: 'CSS',
+    },
+    {
+      skillname: 'Reactjs',
+    },
+  ],
+  portfolio: [
+    {
+      name: 'project1',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/phone.jpg',
+    },
+    {
+      name: 'project2',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/project.jpg',
+    },
+    {
+      name: 'project3',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/project2.png',
+    },
+    {
+      name: 'project4',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/phone.jpg',
+    },
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+  ],
+};
+
+export default resumeData;
