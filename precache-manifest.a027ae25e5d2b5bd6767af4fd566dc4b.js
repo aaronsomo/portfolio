68bc7388f9bb64fefaa5fa03d778cafa293c12e3
Aaron Southammavong <aaroneight07@gmail.com>
2020-05-27 16:48:15 -0500

    Updates

--- precache-manifest.a027ae25e5d2b5bd6767af4fd566dc4b.js
+++ precache-manifest.a027ae25e5d2b5bd6767af4fd566dc4b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "31357bcd3c4e4880aabb",
-    "url": "/static/js/main.31357bcd.chunk.js"
+    "revision": "1c99b27727c518805b53",
+    "url": "/static/js/main.1c99b277.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
     "url": "/static/js/1.6eb6405c.chunk.js"
   },
   {
-    "revision": "31357bcd3c4e4880aabb",
+    "revision": "1c99b27727c518805b53",
     "url": "/static/css/main.14196dee.chunk.css"
   },
   {
-    "revision": "9ff308c6179560b1105e26b3b84f8200",
+    "revision": "535ce9ce0b5dbee5a959681d5216d387",
     "url": "/index.html"
   }
 ];
