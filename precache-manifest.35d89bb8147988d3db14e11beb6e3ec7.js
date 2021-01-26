a9f1f8a57cd97cb7612f8cfab07c9b5faa7b2099
Aaron Southammavong <aaroneight07@gmail.com>
2021-01-26 15:21:13 -0600

    Updates

--- precache-manifest.35d89bb8147988d3db14e11beb6e3ec7.js
+++ precache-manifest.35d89bb8147988d3db14e11beb6e3ec7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "ff3fb1be267d853953cb",
-    "url": "/static/js/main.ff3fb1be.chunk.js"
+    "revision": "e4c58d0f9be6144c7fce",
+    "url": "/static/js/main.e4c58d0f.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
     "url": "/static/js/1.6eb6405c.chunk.js"
   },
   {
-    "revision": "ff3fb1be267d853953cb",
+    "revision": "e4c58d0f9be6144c7fce",
     "url": "/static/css/main.14196dee.chunk.css"
   },
   {
-    "revision": "5b3f92f05fee2cef3e3090790a4f729e",
+    "revision": "d0bbd4f19a669376745817f69156bd97",
     "url": "/index.html"
   }
 ];
